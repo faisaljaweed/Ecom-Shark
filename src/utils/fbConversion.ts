d78f7765c6dc0a8f5544f7b073d9925919fab002
Muhammad Faisal Jawed <faisaljawed2000@gmail.com>
2025-10-07 14:17:41 +0500

    add vercel api

--- src/utils/fbConversion.ts
+++ src/utils/fbConversion.ts
@@ -4,18 +4,21 @@ export const sendFacebookEvent = async (
   value?: number
 ): Promise<void> => {
   try {
-    const response = await fetch("http://localhost:5000/fb-conversion", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        event_name: eventName,
-        email: email,
-        custom_data: {
-          currency: "USD",
-          value: value,
-        },
-      }),
-    });
+    const response = await fetch(
+      "https://fb-conversion-api-three.vercel.app/fb-conversion",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          event_name: eventName,
+          email: email,
+          custom_data: {
+            currency: "USD",
+            value: value,
+          },
+        }),
+      }
+    );
 
     const data = await response.json();
     console.log("✅ Facebook Conversion Response:", data);
